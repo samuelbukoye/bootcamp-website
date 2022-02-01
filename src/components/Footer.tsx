19b13ff27f00d6cdb22adf1d6228bf49fc903f8b
Bukoye Samuel <bukoyesamuel9@gmail.com>
2022-02-01 20:10:43 +0100

    some edits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,7 @@ const Footer = () => {
   return (
     <footer className="p-5 bg-dark text-white text-center position-relative">
       <div className="container">
-        <p className="lead">
-          Copyright &copy; 2022 Bootcamp website || Bukoye Samuel:
-          bukoyesamuel9@gmail.com
-        </p>
+        <p className="lead">Bukoye Samuel || bukoyesamuel9@gmail.com</p>
         <a href="#" className="position-absolute bottom-0 end-0 p-5">
           <i className="bi bi-arrow-up-circle h1"></i>
         </a>
